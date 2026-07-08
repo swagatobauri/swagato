84e91a316b7b96dad6a6bbca930e282bb78a8c76
swagatobauri <swagato731123@gmail.com>
2026-07-09 01:20:58 +0530

    Update layout to force light mode by default

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
         <script dangerouslySetInnerHTML={{
           __html: `
             try {
-              if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+              if (localStorage.theme === 'dark') {
                 document.documentElement.classList.add('dark')
               } else {
                 document.documentElement.classList.remove('dark')
